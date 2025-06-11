feff71ddae929c778497fd0982d9aeb56f293b96
ignazio abbate <ignazio.abbate95@gmail.com>
2025-06-11 18:14:53 +0200

    refactor: simplify Summary component styling by removing unnecessary classes

--- app/(homepage)/(components)/Summary.tsx
+++ app/(homepage)/(components)/Summary.tsx
@@ -2,7 +2,7 @@
 
 export function Summary({ totalTaxes, netIncome }: {totalTaxes: number, netIncome: number}) {
     return (
-      <div className="pt-4 border-t mt-4 space-y-2 bg-gray-50 p-4 rounded-xl shadow-inner">
+      <div className="mt-4 space-y-2 rounded-xl">
         <p className="text-lg font-semibold text-blue-800">Tasse Totali: € {totalTaxes.toFixed(2)}</p>
         <p className="text-lg font-semibold text-green-800">Reddito Netto: € {netIncome.toFixed(2)}</p>
       </div>
