f7b142293ce6a4fa5c7e55607200d43f9c73a71c
ignazio abbate <ignazio.abbate95@gmail.com>
2025-06-11 21:36:45 +0200

    refactor: comment out unused navigation items in site configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
               {children}
             </main>
             <footer className="w-full flex items-center justify-center py-3">
-              <Link
+              {/* <Link
                 isExternal
                 className="flex items-center gap-1 text-current"
                 href="https://heroui.com?utm_source=next-app-template"
@@ -56,7 +56,7 @@ export default function RootLayout({
               >
                 <span className="text-default-600">Powered by</span>
                 <p className="text-primary">HeroUI</p>
-              </Link>
+              </Link> */}
             </footer>
           </div>
         </Providers>
